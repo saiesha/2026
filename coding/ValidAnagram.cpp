569ee3691df6174295a6304ebc9ab3e76f234008
Saiesha <chsaiesha@gmail.com>
2026-02-04 10:31:00 +0530

    Implement isAnagram function in ValidAnagram.cpp
    https://leetcode.com/problems/valid-anagram/

---
+++ coding/ValidAnagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp;
+
+        if(s.size() != t.size())
+        return false;
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            mp[s[i]]++;
+        }
+        for(int i = 0; i < t.size(); i++)
+        {
+            mp[t[i]]--;
+        }
+        for(auto i = mp.begin(); i != mp.end(); i++)
+        {
+            if(i->second != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
