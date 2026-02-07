a27689a12d20c6c72cd975a5199950c79514894d
Saiesha <chsaiesha@gmail.com>
2026-02-07 23:17:52 +0530

    Implement validWordSquare function in Solution class
    https://leetcode.com/problems/valid-word-square/

---
+++ coding/valid-word-square.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        int m = words.size();
+        
+        for(int i = 0; i < m; i++)
+        {
+            int n = words[i].size();
+
+            for(int j = 0; j < n; j++)
+            {
+                if((j < m) && (i < words[j].size() && words[i][j] == words[j][i]))
+                {
+                    continue;
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+};
