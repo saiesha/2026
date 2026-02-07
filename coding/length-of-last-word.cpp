5864cc7e805dd652f83b68c219a78f2190ade803
Saiesha <chsaiesha@gmail.com>
2026-02-07 17:37:45 +0530

    Create length-of-last-word.cpp
    https://leetcode.com/problems/length-of-last-word/

---
+++ coding/length-of-last-word.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+
+        int i = s.size() - 1;
+
+        while((i >= 0)&&(s[i] == ' '))
+        {
+            i--;
+        }
+        while(i >= 0)
+        {
+            if(s[i] == ' ')
+            {
+                break;
+            }
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+};
