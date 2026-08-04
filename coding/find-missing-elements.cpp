2ea4f0bfd75bcf4cf825c6720720783bb12be7ed
Saiesha <chsaiesha@gmail.com>
2026-08-05 02:23:12 +0530

    https://leetcode.com/problems/find-missing-elements alternate solution
    Added an alternate solution using sorting and enumeration to find missing elements.

--- coding/find-missing-elements.cpp
+++ coding/find-missing-elements.cpp
@@ -22,3 +22,23 @@ public:
         return ans;
     }
 };
+
+/* Alternate solution: Using sorting + enumeration
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            for(int j = nums[i]+1; j < nums[i+1]; j++)
+            {
+                ans.push_back(j);
+            }
+        }
+
+        return ans;
+    }
+};
+*/
